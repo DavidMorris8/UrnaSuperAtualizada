dcc0cc788b83accd87f3e32e7069f64a7ad7b61d
DavidMorris8 <110915840+DavidMorris8@users.noreply.github.com>
2023-05-30 17:41:47 -0300

    Add files via upload

---
+++ ProjetoUrnaFinal/ProjetoFinalUrna/src/Interface/Interface.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interface;
+
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author David
+ */
+public interface Interface {
+    public void mostrarPopUpMenu(MouseEvent e);
+    public void novaVotacao();
+    public void limparTela();
+    public void aparecerInformações();
+    public void computarVotos();
+    public void exibirVotos();
+    public void soCandidato();
+    public void listaCandidatos();
+    public void limparListaCandidatos();
+}
